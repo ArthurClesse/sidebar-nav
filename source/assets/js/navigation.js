9f818d56f017d29e850586f85f4abee7a60ab2e0
Arthur Clesse <clessearthur@gmail.com>
2022-02-17 13:20:08 +0100

    Finish component

--- source/assets/js/navigation.js
+++ source/assets/js/navigation.js
@@ -1,6 +1,25 @@
 const btnResize = document.querySelector(".btn__resize");
 const navigation = document.querySelector(".navigation");
+const dropdowns = document.querySelectorAll(".js-dropdown");
 
 btnResize.addEventListener('click', () => {
     navigation.classList.toggle("-small");
 })
+
+dropdowns.forEach( dropdown => {
+    dropdown.addEventListener('click', () => {
+        dropdown.classList.toggle("open");
+        dropdown.classList.toggle("active");
+    })
+})
+
+function removeClassOpen() {
+    dropdowns.forEach(dropdown => {
+        dropdown.classList.remove('open');
+    })
+}
+function removeClassActive() {
+    dropdowns.forEach(dropdown => {
+        dropdown.classList.remove('active');
+    })
+}
